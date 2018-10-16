404dadcbb1f7ed2aac656539df00dde3d9c20a6a
Lorenzo Monacelli <mesonepigreco@gmail.com>
2018-10-17 00:12:05 +0200

    BUGFIX on I/O Utility

--- Modules/Utilities.py
+++ Modules/Utilities.py
@@ -106,7 +106,7 @@ class IOInfo:
         """
         
         # Generate the supercell in real space
-        dyn_sc = minim.dyn.GetRealSpaceFC( minim.ens.supercell )
+        dyn_sc = minim.dyn.GetRealSpaceFC( minim.ensemble.supercell )
 
         # Dyagonalize
         w, pols = dyn_sc.DyagDinQ(0)
