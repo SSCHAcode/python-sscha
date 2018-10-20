27d97674e6f640c9a337d5762c5d8562cfd1a596
Lorenzo Monacelli <mesonepigreco@gmail.com>
2018-10-20 16:29:52 +0200

    NOW STRESS OPTIMIZER SEEMS TO WORK

--- Examples/TestBFGS/TestBFGSOptimizer.py
+++ Examples/TestBFGS/TestBFGSOptimizer.py
@@ -7,23 +7,25 @@ using a custom stress tensorm function
 import numpy as np
 import sscha, sscha.Optimizer
 
-
+I = np.eye(3)
 def free_energy(uc):
     """
     Free energy sample function 
     """
-    return np.trace((uc.dot(np.diag([1,10,100])) - eye(3)).dot(uc.dot(np.diag([1,10,100])) - eye(3)))
+    return np.trace( (uc - I).dot( np.transpose(uc - I)))
+
 def stress(uc):
     """
     Compute the stress tensor of the given free energy
     """
-    return 2 * (uc.dot(np.diag([1,10,100])) - eye(3))
+    Omega = np.linalg.det(uc)
+    return - 2 * uc.dot(uc - I) / Omega
 
 
 # Initialize a random unit cell
 uc = np.random.uniform(size=(3,3))
 
-N_STEPS = 30
+N_STEPS = 10
 opt = sscha.Optimizer.BFGS_UC()
 for i in range(N_STEPS):
     print "---------- STEP %d ----------" % i
