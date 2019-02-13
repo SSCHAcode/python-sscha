8ca16fecc7c92aa9814cb53da50529cc73d2d328
Lorenzo Monacelli <mesonepigreco@gmail.com>
2019-02-13 14:32:25 +0100

    Now the dynamical calculation on the bubble works

--- Examples/TestDynamicalBubble/dynamical_bubble.py
+++ Examples/TestDynamicalBubble/dynamical_bubble.py
@@ -1,7 +1,11 @@
+import sys
 import numpy as np
 
 import cellconstructor as CC
 import cellconstructor.Phonons
+
+from matplotlib.pyplot import *
+
 
 import sscha, sscha.Ensemble
 
@@ -42,4 +46,44 @@ w_odd, p = odd_dyn.DyagDinQ(0)
 # Compare the frequencies
 print "\n".join(["%16.8f  | %16.8f cm-1" % (w_dyn[x] * CC.Phonons.RY_TO_CM, w_odd[x] * CC.Phonons.RY_TO_CM) for x in range(len(w_dyn))])
 
+print ""
 
+# Now get the spectral function
+w_s = np.linspace(0, 3500, 10000)
+nat = dyn.structure.N_atoms
+spectral_function = np.zeros(len(w_s), dtype = np.float64)
+I = np.eye(3*nat, dtype  = np.complex128)
+
+
+self_energies = ens.get_dynamical_bubble(q_gamma, w_s/CC.Phonons.RY_TO_CM, smearing = 10/CC.Phonons.RY_TO_CM)
+
+# Get the mass array
+m = dyn.structure.get_masses_array()
+m = np.tile(m, (3, 1)).T.ravel() # Stretch in a 3*nat array
+for i,_w_ in enumerate(w_s):
+    w = _w_ / CC.Phonons.RY_TO_CM
+
+    # Get the dynamical force constant
+    new_dyn = dyn.dynmats[0] + self_energies[i]
+
+    # Convert in a dynamical matrix
+    new_dyn /= np.sqrt(np.einsum("a,b", m, m))
+    
+    g_minus_one = w**2 * I - new_dyn
+
+    green_matrix = np.linalg.inv(g_minus_one)
+    # - Im Tr(G)
+    spectral_function[i] = np.einsum("aa", -np.imag(green_matrix))
+
+    if i % 5 == 0:
+        sys.stdout.write("\rStatus %d %%" %( i * 100 / len(w_s)))
+print ""
+    
+# Plot the spectral function
+figure(dpi = 170)
+title("Spectral function")
+xlabel("Frequency [cm-1]")
+ylabel("Spectral function")
+plot(w_s, spectral_function)
+tight_layout()
+show()
