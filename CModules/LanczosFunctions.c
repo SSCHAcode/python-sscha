dde4bba9eba8bae81e3aa0bfe428b67dd6522ba3
Lorenzo Monacelli <mesonepigreco@gmail.com>
2020-05-27 13:45:25 +0200

    Bugfix on index_Y and index_A

--- CModules/LanczosFunctions.c
+++ CModules/LanczosFunctions.c
@@ -1655,7 +1655,7 @@ int index_Y(int a, int b, int N) {
 	// Get the symmetric index
 	int ret = N;
 
-	if (a >= b) ret += ((a - 1)*a) / 2 + b;
+	if (a < b) ret += ((a - 1)*a) / 2 + b;
 	else ret += ((b - 1)*b) / 2 + a;
 
 	return ret;
@@ -1669,7 +1669,7 @@ int index_A(int a, int b, int N) {
 	// Go to Start-A
 	ret += ((N+1)*N) / 2;
 
-	if (a >= b) ret += ((a - 1)*a) / 2 + b;
+	if (a < b) ret += ((a - 1)*a) / 2 + b;
 	else ret += ((b - 1)*b) / 2 + a;
 
 	return ret;
