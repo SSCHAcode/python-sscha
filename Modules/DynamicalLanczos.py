Now the test runs (but it gives a wrong result)

Modules/DynamicalLanczos.py
@@ -133,6 +133,11 @@ class Lanczos:
         self.initialized = False
         self.perturbation_modulus = 1
         self.q_vectors = None # The q vectors of each mode
+        self.dyn = None
+        self.uci_structure = None
+        self.super_structure = None
+        self.qe_sym = None
+        self.L_linop = None
 
         # Perform a bare initialization if the ensemble is not provided
         if ensemble is None:
@@ -255,7 +260,33 @@ class Lanczos:
         # fasten the convergence of low energy modes
         self.reverse_L = False
         self.shift_value = 0
-        self.symmetrize = False
+
+
+        # Setup the attribute control
+        self.__total_attributes__ = [item for item in self.__dict__.keys()]
+        self.fixed_attributes = True # This must be the last attribute to be setted
+
+
+    def __setattr__(self, name, value):
+        """
+        This method is used to set an attribute.
+        It will raise an exception if the attribute does not exists (with a suggestion of similar entries)
+        """
+        
+        if "fixed_attributes" in self.__dict__:
+            if name in self.__total_attributes__:
+                super(Lanczos, self).__setattr__(name, value)
+            elif self.fixed_attributes:
+                similar_objects = str( difflib.get_close_matches(name, self.__total_attributes__))
+                ERROR_MSG = """
+        Error, the attribute '{}' is not a member of '{}'.
+        Suggested similar attributes: {} ?
+        """.format(name, type(self).__name__,  similar_objects)
+
+                raise AttributeError(ERROR_MSG)
+        else:
+            super(Lanczos, self).__setattr__(name, value)
+        
 
 
     def reset(self):
@@ -765,11 +796,13 @@ This may be caused by the Lanczos initialized at the wrong temperature.
             transpose : bool
                 If True, the transpose of L is computed.
         """
+        #print("Starting with psi:", self.psi)
 
         Y1 = self.get_Y1()
         R1 = self.psi[: self.n_modes]
 
         weights = np.zeros(self.N, dtype = np.double)
+
 
         # Create the multiplicative matrices for the rest of the anharmonicity
         n_mu = 0
@@ -784,12 +817,12 @@ This may be caused by the Lanczos initialized at the wrong temperature.
 
             # The equation is
             # Y^(1)_new = 2 Ya Yb^2 Y^(1) + 2 Yb Ya^2 Y^(1)
-            coeff_Y = np.einsum("a, b, b- > ab", Y_w, Y_w, Y_w)
-            coeff_Y += np.einsum("a, a, b- > ab", Y_w, Y_w, Y_w)
+            coeff_Y = np.einsum("a, b, b -> ab", Y_w, Y_w, Y_w)
+            coeff_Y += np.einsum("a, a, b -> ab", Y_w, Y_w, Y_w)
             coeff_Y *= 2
 
-            coeff_RA = np.einsum("a, b, b- > ab", Y_w, ReA_w, Y_w)
-            coeff_RA += np.einsum("a, a, b- > ab", Y_w, ReA_w, Y_w)
+            coeff_RA = np.einsum("a, b, b -> ab", Y_w, ReA_w, Y_w)
+            coeff_RA += np.einsum("a, a, b -> ab", Y_w, ReA_w, Y_w)
             coeff_RA += 2
 
             # Get the new perturbation
@@ -799,16 +832,30 @@ This may be caused by the Lanczos initialized at the wrong temperature.
             Y1 = Y1_new
 
 
+        #print("X:", self.X)
+        #print("w:", self.w)
+        #print ("R1:", R1)
+        #print("Y1:", Y1)
+        #print("T:", self.T)
+
 
         # Get the weights of the perturbation (psi vector)
         sscha_HP_odd.GetWeights(self.X, self.w, R1, Y1, self.T, weights)
+
+        #print("Weights:", weights)
 
         # Get the averages on the perturbed ensemble
         w_is = np.tile(self.rho, (self.n_modes, 1)).T
         w_1 = np.tile(weights, (self.n_modes, 1)).T
 
+        #print("rho shape:", np.shape(self.rho))
+        #print("Shape w_is:", np.shape(w_is))
+
         # The force average
-        f_pert_av = np.sum(self.Y * w_is * w_1, axis = 0) / self.N_eff
+        avg_numbers = self.Y * w_is *  w_1 #np.einsum("ia, i, i -> ia", self.Y, w_is, w_1)
+        #print("Shape Y:", np.shape(avg_numbers))
+        f_pert_av = np.sum(avg_numbers, axis = 0) / self.N_eff
+        #print("Shape F:", np.shape(f_pert_av))
 
         d2v_pert_av = np.zeros((self.n_modes, self.n_modes), dtype = np.double, order = "C")
         sscha_HP_odd.Get_D2DR2_PertV(self.X, self.Y, self.w, self.rho, weights, self.T, d2v_pert_av)
@@ -841,6 +888,8 @@ This may be caused by the Lanczos initialized at the wrong temperature.
             final_psi[current : current + self.n_modes - i] = pert_RA[i, i:]
             current = current + self.n_modes - i
 
+
+        #print("Output:", final_psi)
         return final_psi
 
 
@@ -2294,6 +2343,16 @@ Max number of iterations: {}
 
             # Get the a coefficient
             a_coeff = psi_p.dot(L_q)
+
+            # Check if something whent wrong
+            if np.isnan(a_coeff):
+                ERR_MSG = """
+Invalid value encountered during the Lanczos.
+Check if you have correctly initialized the algorithm.
+This may happen if the SCHA matrix has imaginary or zero frequencies,
+or if the acoustic sum rule is not satisfied.
+"""
+                raise ValueError(ERR_MSG)    
 
             # Get the two residual vectors
             rk = L_q - a_coeff * psi_q 

tests/test_fast_lanczos_1D/test_1d_fast.py
@@ -4,10 +4,16 @@ from __future__ import division
 import sscha, sscha.DynamicalLanczos
 import numpy as np
 
+import scipy
+import scipy.optimize
+
+
+import matplotlib.pyplot as plt
+
 def sscha_1d(func, x, m = 1, return_res = False):
     # Get the sscha value
     #_xreal_ = random.normal(0, 1, size = 5000)
-    _xreal_ = linspace(-5, 5, 10000)
+    _xreal_ = np.linspace(-5, 5, 10000)
     def complete_sscha(phi):
         if phi < 0:
             return 1000000 + phi**2
@@ -16,7 +22,7 @@ def sscha_1d(func, x, m = 1, return_res = False):
         dx_ = _x_[1]- _x_[0]
         
         #pot_energy = sum(func(_x_)) / 5000
-        pot_energy = sum( func(_x_) * exp(-(_x_ - x)**2 / (2*phi**2))) *dx_ / sqrt(2*pi*phi**2)
+        pot_energy = np.sum( func(_x_) * np.exp(-(_x_ - x)**2 / (2*phi**2))) *dx_ / np.sqrt(2*np.pi*phi**2)
         
         return kin_energy + pot_energy
     
@@ -28,13 +34,13 @@ def sscha_1d(func, x, m = 1, return_res = False):
     return  complete_sscha(res.x[0])
 
 def sscha_rho(_x_, x0, phi0):
-    return exp(-(_x_ - x0)**2 / (2*phi0**2))/ sqrt(2*pi*phi0**2)
+    return np.exp(-(_x_ - x0)**2 / (2*phi0**2))/ np.sqrt(2*np.pi*phi0**2)
 
 def sscha_w(m, phi0):
     return 1 / (2*m * phi0**2)
 
 def sscha_full(func, x0 = 1, phi0 = 1, m = 1):
-    _xreal_ = linspace(-5, 5, 10000)
+    _xreal_ = np.linspace(-5, 5, 10000)
     def complete_sscha(params):
         x, phi = params
         if phi < 0:
@@ -45,7 +51,7 @@ def sscha_full(func, x0 = 1, phi0 = 1, m = 1):
         
         #pot_energy = sum(func(_x_)) / 5000
         rho = sscha_rho(_x_, x, phi)
-        pot_energy = sum( func(_x_) * rho) * dx_
+        pot_energy = np.sum( func(_x_) * rho) * dx_
         
         return kin_energy + pot_energy
     
@@ -178,7 +184,7 @@ def DynamicalSSCHA(func,w_array, x0, phi0, _x_, full_spectral = False, m = 1, sm
     
 
 
-def test_lanczos_1d():
+def test_lanczos_1d(plot = False):
     """
     This is the proper testing function.
     Here we are going to apply the SSCHA in the one dimensional BO energy landscape.
@@ -195,10 +201,11 @@ def test_lanczos_1d():
     # Initialize the lanczos algorithm
     lanc = sscha.DynamicalLanczos.Lanczos()
 
-
     # Initliaize the lanczos from a 1D problem
-    lanc.w = np.zeros([1], dtype = np.double)
+    lanc.w = np.zeros(1, dtype = np.double)
     lanc.w[0] = w
+    lanc.n_modes = 1
+    lanc.pols = np.ones((1,1), dtype = np.double)
     lanc.m = np.array([np.double(1)])
 
     # get the N random configuration
@@ -206,13 +213,42 @@ def test_lanczos_1d():
     N_RANDOM = 1000
     u_ensemble = np.double(np.random.normal(x_c, phi, size = (N_RANDOM, 1)))
     f = bo_force(u_ensemble) - sscha_force(u_ensemble, x_c, phi, m = 1)
-    lanc.rho = np.ones(u_ensemble.shape, dtype = np.double)
+    lanc.rho = np.ones(N_RANDOM, dtype = np.double)
     lanc.N = N_RANDOM
+    lanc.N_eff = N_RANDOM
     lanc.X = u_ensemble
     lanc.Y = f
-
+    lanc.psi = np.zeros(3, dtype = np.double)
 
     # Set the temperature
     lanc.T = 0
 
-    
+    # Prepare the 1D phonon green function
+    lanc.prepare_perturbation(np.ones(1, dtype = np.double), masses_exp = 0)
+
+    # Fake the algorithm with a false initialization
+    lanc.initialized = True
+
+     # Prepare the L as a linear operator (Prepare the possibility to transpose the matrix)
+    def L_transp(psi):
+        return lanc.apply_full_L(psi, transpose= True, force_FT = True)
+    def L_direct(psi):
+        return lanc.apply_full_L(psi, transpose= False, force_FT = True)
+    lanc.L_linop = scipy.sparse.linalg.LinearOperator(shape = (len(lanc.psi), len(lanc.psi)), matvec = L_direct, rmatvec = L_transp, dtype = np.double)
+
+
+    # Compute the Lanczos algorithm (biconjugate)
+    lanc.run_FT(10)
+
+    # Get the green function
+    w_array = np.linspace(0, 10, 1000)
+    gf = lanc.get_green_function_continued_fraction(w_array, smearing = 0.1, use_terminator = False)
+
+    if plot:
+        plt.plot(w_array, -np.imag(gf))
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    test_lanczos_1d(plot = True)