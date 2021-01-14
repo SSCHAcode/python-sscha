The on-the-fly symmetrization seems to work correctly now I also fixed a bug on the average atomic force (when the gradient was not exactly zero)

Modules/DynamicalLanczos.py
@@ -219,12 +219,17 @@ Error, 'select_modes' should be an array of the same lenght of the number of mod
 
         # Get the average force
         f_mean = ensemble.get_average_forces(get_error = False)
+
+        # Perform the symmetrization of the average force
+        qe_sym = CC.symmetries.QE_Symmetry(self.dyn.structure)
+        qe_sym.SetupQPoint()
+        qe_sym.SymmetrizeVector(f_mean)
+
+        # Reproduce the average force on the full supercell
         f_mean = np.tile(f_mean, (np.prod(ensemble.current_dyn.GetSupercell()), 1)).ravel()
 
-        # Subtract the SSCHA gradient on nuclei
-        # This allows the correct calculation also if the average force is not zero
-        print(np.shape(f), np.shape(f_mean))
-        f[:, :] -= np.tile(f_mean, (self.N, 1))
+        # Transpform in Bohr
+        f_mean *= Ensemble.Bohr
         
         # Subtract also the average force to clean more the stochastic noise
         #av_force = ensemble.get_average_forces(get_error = False).ravel()
@@ -240,6 +245,11 @@ Error, 'select_modes' should be an array of the same lenght of the number of mod
 
             u /= Ensemble.Bohr
             f *= Ensemble.Bohr
+
+        # Subtract the SSCHA GRADIENT on average position
+        # In this way the calculation works even if the system is not in equilibrium
+        print(np.shape(f), np.shape(f_mean))
+        f[:, :] -= np.tile(f_mean, (self.N, 1))
 
 
         # Perform the mass rescale to get the tilde variables
@@ -359,7 +369,7 @@ Error, 'select_modes' should be an array of the same lenght of the number of mod
         self.arnoldi_matrix = [] # If requested, the upper triangular arnoldi matrix
 
 
-    def init(self, use_symmetries = False):
+    def init(self, use_symmetries = True):
         """
         INITIALIZE THE CALCULATION
         ==========================
@@ -369,7 +379,7 @@ Error, 'select_modes' should be an array of the same lenght of the number of mod
         Parameters
         ----------
             use_symmetries : bool
-                if False (default True) symmetries are neglected.
+                if False (default True) symmetries are neglected (unless the ensemble has been unwrapped).
     
         """
         self.prepare_symmetrization(no_sym = not use_symmetries)
@@ -1070,12 +1080,14 @@ This may be caused by the Lanczos initialized at the wrong temperature.
 
         # Compute the perturbed averages (the time consuming part is HERE)
         #print("Entering in get pert...")
-        sscha_HP_odd.GetPerturbAverage(self.X, self.Y, self.w, self.rho, R1, Y1, self.T, apply_d4, f_pert_av, d2v_pert_av)
+        sscha_HP_odd.GetPerturbAverageSym(self.X, self.Y, self.w, self.rho, R1, Y1, self.T, apply_d4, 
+                                          self.symmetries, self.N_degeneracy, deg_space_new, 
+                                          f_pert_av, d2v_pert_av)
         #print("Out get pert")
 
-        #print("<f> pert = {}".format(f_pert_av))
-        #print("<d2v/dr^2> pert = {}".format(d2v_pert_av))
-        #print()
+        print("<f> pert = {}".format(f_pert_av))
+        print("<d2v/dr^2> pert = {}".format(d2v_pert_av))
+        print()
 
         # Compute the average with the old version
         if use_old_version:

tests/test_fast_lanczos_snse/test_fast_lanczos_snse.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import numpy as np
 
 import cellconstructor as CC
@@ -9,6 +11,75 @@ import sscha.Ensemble
 import scipy, scipy.sparse
 
 import sys, os
+
+def test_lanczos_symmetries(temperature = 250, N = 10000):
+    """
+    Test if the ensemble uwrapping works as the symmetries in the polarization
+    space.
+    """
+    total_path = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(total_path)
+
+    PATH_TO_DYN = "../../Examples/TestLanczosFiniteTemperature/SnTe_test/T_{}_N_{}".format(temperature, N)
+
+    if not os.path.exists(PATH_TO_DYN):
+        raise IOError("Error, the path {} does not exist, please change temperature or N".format(PATH_TO_DYN))
+
+    # Load the ensemble
+    dyn = CC.Phonons.Phonons(os.path.join(PATH_TO_DYN, "SnTe_final"), 3)
+    ens = sscha.Ensemble.Ensemble(dyn, temperature, dyn.GetSupercell())
+    ens.load_bin(os.path.join(PATH_TO_DYN, "ensemble"), 1)
+
+    dirname = "SC_T_{:d}_N_{:d}".format(temperature, N)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+
+    # Get only the first configuration (this is the best way to test everything)
+    first_config = np.zeros(ens.N, dtype = bool)
+    first_config[:1] = True
+    new_ens = ens.split(first_config)
+
+    TEST_V3 = True
+    TEST_V4 = False
+
+    lanc1 = sscha.DynamicalLanczos.Lanczos(new_ens, unwrap_symmetries=True)
+    lanc1.ignore_v3 = not TEST_V3
+    lanc1.ignore_v4 = not TEST_V4
+    lanc1.init()
+
+    lanc2 = sscha.DynamicalLanczos.Lanczos(new_ens, unwrap_symmetries=False)
+    lanc2.ignore_v3 = not TEST_V3
+    lanc2.ignore_v4 = not TEST_V4
+    lanc2.init()
+
+    # Get a random vector
+    v1 = np.random.uniform(size = lanc1.psi.shape)
+    lanc1.psi[:] = v1
+    lanc2.psi[:] = v1
+
+    print("ALL THE UNWRAPPED FORCES:")
+    print(lanc1.Y)
+
+    print("ALL THE UNWRAPPED FORCES 2:")
+    print(lanc2.Y)
+
+    # Apply L
+    print("APPLY UNWRAPPED.")
+    sys.stdout.flush()
+    w1 = lanc1.apply_anharmonic_FT()
+
+    print("ALL THE UNWRAPPED FORCES AGAIN:")
+    print(lanc2.Y)
+    
+    print("APPLY DIRECT SYMMETRIZATION")
+    sys.stdout.flush()
+    w2 = lanc2.apply_anharmonic_FT()
+
+    disp = np.max(np.abs(w2 - w1))
+    assert disp < 1e-12, "Error, the symmetrization in LANCZOS does not work properly."
+    
+    
 
 def test_lanczos_snse(temperature = 250, N = 10000):
     total_path = os.path.dirname(os.path.abspath(__file__))
@@ -41,16 +112,16 @@ def test_lanczos_snse(temperature = 250, N = 10000):
     
 
     # Prepare the Lanczos
-    lanczos = sscha.DynamicalLanczos.Lanczos(new_ens, unwrap_symmetries=True)
+    lanczos = sscha.DynamicalLanczos.Lanczos(new_ens, unwrap_symmetries=False)
     lanczos.ignore_v3 = False
     lanczos.ignore_v4 = False
     lanczos.init()
 
-    print("Computing L matrix...")
-    L = sscha.DynamicalLanczos.get_full_L_matrix(lanczos, transpose = False)
-    np.save(os.path.join(dirname, "L_good_final.npy"), L)
-    print("Saved L matrix.")
-    exit()
+    # print("Computing L matrix...")
+    # L = sscha.DynamicalLanczos.get_full_L_matrix(lanczos, transpose = False)
+    # np.save(os.path.join(dirname, "L_good_final.npy"), L)
+    # print("Saved L matrix.")
+    # exit()
     
     # # Try to diagonalize using scipy to check eigenvalues and eigenvectors:
     # print("Going into scipy")
@@ -82,6 +153,10 @@ def test_lanczos_snse(temperature = 250, N = 10000):
 
     lanczos.save_status("LanczosSnTe_v4.npz")
 
+    # Test the frequency
+    w, pols = hessian.DiagonalizeSupercell()
+    assert np.abs(w[3] - np.sqrt(np.abs(w2))) * CC.Units.RY_TO_CM < 1e-3, "Error, the lanczos w -> 0 does not match the hessian matrix with Bianco algorithm" 
+
     # # Get the free energy hessian
     # hessian = lanczos.run_biconjugate_gradient(algorithm = "bicgstab", use_preconditioning = True, tol = 1e-12)
 
@@ -92,6 +167,6 @@ def test_lanczos_snse(temperature = 250, N = 10000):
     # print("\n".join(["{} cm-1".format(np.sign(x) * np.sqrt(np.abs(x)) * CC.Units.RY_TO_CM) for x in w]))
 
 if __name__ == "__main__":
+    test_lanczos_symmetries()
     test_lanczos_snse()
-    
     