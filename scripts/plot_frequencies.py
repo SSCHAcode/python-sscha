1b6f4711e127e205df17c800d8007eadf456a211
Lorenzo Monacelli <mesonepigreco@gmail.com>
2021-02-12 15:49:17 +0100

    Now plot_frequencies should work

--- scripts/plot_frequencies.py
+++ scripts/plot_frequencies.py
@@ -6,8 +6,8 @@ import sys
 
 RY_TO_CM = 109691.40235
 
-if len(sys.argv) <= 2:
-    print "Specify the frequency files."
+if len(sys.argv) < 2:
+    print ("Specify the frequency files.")
     exit(1)
 
 freq_data = concatenate([loadtxt(sys.argv[x]) for x in range(1, len(sys.argv))])
