7333dc9ab99cfa55868a1dc208173b4d3a72b6e5
Lorenzo Monacelli <mesonepigreco@gmail.com>
2021-05-26 11:00:35 +0200

    Added a fix in get new direction

--- Modules/Optimizer.py
+++ Modules/Optimizer.py
@@ -153,6 +153,8 @@ class UC_OPTIMIZER:
         # Steepest descent
         if self.algorithm == "sd":
             direction = grad
+            self.last_direction = direction.copy()
+            self.last_grad = grad.copy()
         elif self.algorithm == "cg":
             if np.max(np.abs(self.last_direction)) < 1e-10:
                 # First step
@@ -175,7 +177,7 @@ class UC_OPTIMIZER:
         else:
             raise NotImplementedError("Error, cell algorithm setted to {}, but only 'sd' and 'cg' supported.".format(self.algorithm))
                     
-                
+            
 
         return direction
 
