Fixed that the gradient of the structure was printed before the custom gradient was called

Modules/SchaMinimizer.py
@@ -400,19 +400,10 @@ class SSCHA_Minimizer(object):
                 #qe_sym.SymmetrizeFCQ(dyn_grad, np.array(self.dyn.q_stars), asr = "custom")
                 #print "SECOND DIAG:", np.linalg.eigvalsh(dyn_grad[0, :, :])
             
-            # Append the gradient modulus to the minimization info
-            self.__gw__.append(np.sqrt( np.sum(struct_grad**2)))
-            self.__gw_err__.append(np.sqrt( np.einsum("ij, ij", struct_grad_err, struct_grad_err) / qe_sym.QE_nsymq))
-        
             
             # Perform the step for the structure
             #print "min step:", self.min_step_struc
             #self.dyn.structure.coords -= self.min_step_struc * struct_grad
-        else:
-
-            # Append the gradient modulus to the minimization info
-            self.__gw__.append(0)
-            self.__gw_err__.append(0)
 
 
         # Perform the gradient restriction
@@ -420,6 +411,13 @@ class SSCHA_Minimizer(object):
             custom_function_gradient(dyn_grad, struct_grad)    
             
 
+        # Append the gradient modulus to the minimization info
+        if self.minim_struct:
+            self.__gw__.append(np.sqrt( np.sum(struct_grad**2)))
+            self.__gw_err__.append(np.sqrt( np.einsum("ij, ij", struct_grad_err, struct_grad_err) / qe_sym.QE_nsymq))
+        else:
+            self.__gw__.append(0)
+            self.__gw_err__.append(0)
 
         
         # Store the gradient in the minimization

Modules/Utilities.py
@@ -292,8 +292,8 @@ class ModeProjection:
         # Prepare the projector on the structure
         for mu in range(index_mode_start, index_mode_end):
             pvec = np.real(self.pols[:, mu, 0])
-            #pvec /= pvec.dot(pvec)
-            self.proj_vec[:,:] += np.outer(pvec, pvec)
+            pvec /= np.sqrt(pvec.dot(pvec))
+            self.proj_vec[:,:] += np.outer(pvec / _msq_, pvec * _msq_)
                 
 
         # Impose the sum rule 
@@ -316,7 +316,7 @@ class ModeProjection:
 
         # Project the structure in the polarization vectors
         struct_grad_new = self.proj_vec.dot(struct_grad.ravel())
-        struct_grad = struct_grad_new.reshape((self.nat, 3))
+        struct_grad[:,:] = struct_grad_new.reshape((self.nat, 3))
 
         _m_ = np.tile(self.masses, (3, 1)).T.ravel()
 