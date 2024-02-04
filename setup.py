505f72777756c0f546c487ee9d5cb3a651752c47
Lorenzo Monacelli <mesonepigreco@gmail.com>
2024-02-04 13:37:21 +0100

    Reverted the setup to before the Cmake

--- setup.py
+++ setup.py
@@ -1,56 +1,150 @@
-from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
-import os
-import sys
-import subprocess
+from __future__ import print_function
 
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
+from numpy.distutils.core import setup, Extension
+import os, sys 
+import numpy 
 
-class CMakeBuild(build_ext):
-    def run(self):
+extra_flags_c = []
+extra_link_args_c = []
+mpi_compile_args = []
+mpi_link_args = []
+
+
+# If true, do not check for parallel
+avoid_parallel_test = True 
+
+# Get the MPI from environmental variables
+parallel = False 
+if "MPICC" in  os.environ:
+        mpicc = os.environ["MPICC"]
+        parallel = True
+        print ()
+        print("Parallel compiler setted to:", mpicc)
+        print()
+
+# Check for the python parallel libraries
+python_parallel = True
+try:
+        import pypar
+except:
         try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError('CMake must be installed to build the following extensions: ' +
-                               ', '.join(e.name for e in self.extensions))
+                import mpi4py
+        except:
+                #parallel = False
+                python_parallel = False
 
-        for ext in self.extensions:
-            self.build_extension(ext)
+# Setup the parallel environemnt
+if parallel:
+        # If we are here we can compile using MPI support
+        mpi_compile_args = os.popen("%s -show" % mpicc).read().strip().split(' ')[1:]
+        mpi_link_args    = os.popen("%s -show" % mpicc).read().strip().split(' ')[1:]
+        extra_flags_c += ["-D_MPI"]
 
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+# Check if it is python2 or 3
+if sys.version_info[0] < 3:
+        print("Running on python 2, added the flag -D_PYTHON2")
+        extra_flags_c += ["-D_PYTHON2"]
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
 
-        # Assuming CMakeLists.txt is in the same directory as setup.py
-        os.chdir(ext.sourcedir)
-        self.spawn(['cmake', '.'] + cmake_args)
-        if not self.dry_run:
-            self.spawn(['cmake', '--build', '.'] + build_args)
-        os.chdir(self.get_ext_fullpath(ext.name))
 
-setup(
-    name='python-sscha',
-    version='1.4.1',
-    author='Lorenzo Monacelli',
-    description='Python implementation of the sscha code',
-    long_description='',
-    ext_modules=[CMakeExtension('SCHAModules', sourcedir='')],
-    cmdclass=dict(build_ext=CMakeBuild),
-    packages=find_packages(),
-    package_dir={'sscha': 'Modules'},
-    include_package_data=True,
-    package_data={"": ["*.jl"]},
-    install_requires=['numpy', 'ase', 'scipy', 'cellconstructor', 'spglib', 'matplotlib'],
-    scripts=['scripts/sscha', 'scripts/cluster_check.x', 'scripts/plot_frequencies.py',
-             'scripts/sscha-plot-data.py', 'scripts/static-vc-relax.pyx', 'scripts/read_incomplete_ensemble.py'],
-    license='GPLv3',
-    zip_safe=False
-)
 
+LIBRARIES = ["lapack", "blas"]
+EXTRA_F90_FLAGS =  ["-cpp", "-fopenmp"]
+EXTRA_LINK_ARGS = ["-fopenmp"]
+
+# Compile the fortran SCHA modules
+SCHAModules = Extension(name = "SCHAModules", 
+                        sources = ["SCHAModules/module_stochastic.f90",
+                                   "SCHAModules/module_new_thermodynamic.f90",
+                                   "SCHAModules/module_anharmonic.f90",
+                                   "SCHAModules/get_stress_tensor.f90",
+                                   "SCHAModules/get_gradient_supercell.f90",
+                                   "SCHAModules/get_upsilon_matrix.f90",
+                                   "SCHAModules/multiply_lambda_tensor.f90",
+                                   "SCHAModules/cell_force.f90",
+                                   "SCHAModules/get_gradient_supercell_fast.f90",
+                                   "SCHAModules/get_g.f90",
+                                   "SCHAModules/get_emat.f90",
+                                   "SCHAModules/get_v3.f90",
+                                   "SCHAModules/get_odd_straight.f90",
+                                   "SCHAModules/get_cmat.f90",
+                                   "SCHAModules/get_v4.f90",
+                                   "SCHAModules/get_odd_straight_with_v4.f90"],
+                        libraries = LIBRARIES,
+                        extra_f90_compile_args = EXTRA_F90_FLAGS,
+                        extra_link_args= EXTRA_LINK_ARGS)
+
+
+
+
+# Setup the HP performance module
+#odd_HP = Extension(name = "sscha_HP_odd",
+#                   sources= ["CModules/odd_corr_module.c", "CModules/LanczosFunctions.c"],
+#                   include_dirs=[numpy.get_include()],
+#                   extra_compile_args= extra_flags_c + mpi_compile_args,
+#                   extra_link_args = mpi_link_args + extra_link_args_c
+#                   )
+
+
+
+# Prepare the compilation of the Python Conde
+setup( name = "python-sscha",
+       version = "1.4.1",
+       description = "Python implementation of the sscha code",
+       author = "Lorenzo Monacelli",
+       url = "https://github.com/mesonepigreco/python-sscha",
+       packages = ["sscha"],
+       package_dir = {"sscha": "Modules"},
+       include_package_data = True,
+       package_data={"": ["*.jl"]},
+       setup_requires = ["numpy", "ase", "scipy", "cellconstructor", "spglib", "matplotlib"],
+       ext_modules = [SCHAModules], # odd_HP
+       scripts = ["scripts/sscha", "scripts/cluster_check.x", "scripts/plot_frequencies.py",
+                  "scripts/sscha-plot-data.py",
+                  "scripts/static-vc-relax.pyx", "scripts/read_incomplete_ensemble.py"],
+       license = "GPLv3"
+       )
+                                                                                                                                                          
+if not python_parallel and not parallel and not avoid_parallel_test:                                                                                                                      
+        print()                                                                                                                                               
+        print("======= WARNING =======")                                                                                                                      
+        print("Nor python parallel neither MPI compiler found.")                                                                                              
+        print("If you whish to activate MPI acceleration,")                                                                                                   
+        print("Consider installing either pypar or mpi4py")                                                                                                   
+        print("For example, try to run: ")                                                                                                                    
+        print(" >>> MPICC=mpicc python " + " ".join(sys.argv))                                                                                                
+        print("Note: clean the build directory if you whish to recompile the code.")                                                                          
+        print("=======================")                                                                                                                      
+        print()                                                                                                                                               
+elif not parallel and not avoid_parallel_test:
+        print()
+        print("======= WARNING =======")
+        print("No MPI compiler found, please specify MPICC environmental variable")
+        print("For example, try to run: ")
+        print(" >>> MPICC=mpicc python " + " ".join(sys.argv))
+        print("Note: clean the build directory if you whish to recompile the code.")
+        print("=======================")
+        print()
+elif not python_parallel and not avoid_parallel_test:
+        print()
+        print("======= WARNING =======")
+        print("No Python MPI library found")
+        print("Supported libraries:")
+        print(" - pypar ")
+        print(" - mpi4py ")
+        print()
+        print("Note: Fast MPI implemetation will crash if used")
+        print("      consider to install one of these libraries.")
+        print("      (No need to reinstall python-sscha)")
+        print("=======================")
+        print()
+elif not avoid_parallel_test:
+        print()
+        print(" PARALLEL ENVIRONMENT DETECTED CORRECTLY ")
+        print()
+
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
