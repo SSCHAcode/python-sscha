eef1fcbcf5df076c4f73c79c5fb4abdcf5cd98cc
diegomartinez2 <diegomartinez2@yahoo.es>
2024-02-07 12:11:51 +0100

    Undo the test.

--- tests/aiida_ensemble/test_aiida_ensemble.py
+++ tests/aiida_ensemble/test_aiida_ensemble.py
@@ -67,19 +67,19 @@ def test_get_running_workchains(generate_workchain_pw_node):
     assert success == [False, True, False]
 
 
-# @pytest.mark.usefixtures('aiida_profile')
-# def test_submit_and_get_workchains(fixture_code):
-#     """Test the :func:`sscha.aiida_ensemble.submit_and_get_workchains` method."""
-#     from cellconstructor.Structure import Structure
-#     from sscha.aiida_ensemble import submit_and_get_workchains
-#
-#     pw_code = fixture_code('quantumespresso.pw')
-#     structures = [Structure(nat=1) for _ in range(5)]
-#
-#     workchains = submit_and_get_workchains(
-#         structures=structures,
-#         pw_code=pw_code,
-#         temperature=300,
-#     )
-#
-#     assert len(workchains) == 5
+@pytest.mark.usefixtures('aiida_profile')
+def test_submit_and_get_workchains(fixture_code):
+    """Test the :func:`sscha.aiida_ensemble.submit_and_get_workchains` method."""
+    from cellconstructor.Structure import Structure
+    from sscha.aiida_ensemble import submit_and_get_workchains
+
+    pw_code = fixture_code('quantumespresso.pw')
+    structures = [Structure(nat=1) for _ in range(5)]
+
+    workchains = submit_and_get_workchains(
+        structures=structures,
+        pw_code=pw_code,
+        temperature=300,
+    )
+
+    assert len(workchains) == 5
